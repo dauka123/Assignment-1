6968fb98f09e0e86859e07178faed468bee9a05a
amanzhol <rysbek_dauren@mail.ru>
2023-04-07 04:12:25 +0600

    comments added

--- src/Task4.java
+++ src/Task4.java
@@ -10,12 +10,12 @@ public class Task4 {
         if (num == 1){
             return num;
         }
-        num *= findFactorial(num-1);
+        num *= findFactorial(num-1); // find the factorial of number
         return num;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        int num = scanner.nextInt(); // input number
         System.out.println(findFactorial(num));
     }
 }
