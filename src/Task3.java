25b11defcf33fae9fd2a9fa6bb7bd3135099e7bd
amanzhol <rysbek_dauren@mail.ru>
2023-04-07 00:56:27 +0600

    .

---
+++ src/Task3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task3 {
+    /*
+        @ checkPrime - definition of checking prime function
+        @ n - number that need to check for prime
+        @ return - returning result of checking
+    */
+    public static String checkPrime(int num){
+        int x = 1;
+        for (int i = 2; i < num-1; i++){
+            if (num % i == 0) {
+                x += 1; // if number have any other dividers we will know
+            }
+        }
+        if (x == 1){
+            return "Prime";
+        }
+        else{
+            return "Composite";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(checkPrime(num));
+    }
+}
