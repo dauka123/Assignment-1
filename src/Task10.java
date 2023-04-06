0320540975b8ed691d9ed1c1d49024aa5688c045
amanzhol <rysbek_dauren@mail.ru>
2023-04-07 04:17:20 +0600

    comments added

--- src/Task10.java
+++ src/Task10.java
@@ -15,7 +15,7 @@ public class Task10 {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int a = sc.nextInt(); // input numbers
         int b = sc.nextInt();
         System.out.println(findGCD(a, b));
     }
